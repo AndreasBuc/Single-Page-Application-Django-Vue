f8c5fd39f01dd27b7810642c27c328feb6550ec2
AndreasBuc <63713732+AndreasBuc@users.noreply.github.com>
2020-06-21 09:43:27 +0200

    Rest-API done so far

--- QuestionTime/questions/apps.py
+++ QuestionTime/questions/apps.py
@@ -1,5 +1,8 @@
 from django.apps import AppConfig
 
 
-class QuestionsConfig(AppConfig):
+class QuestionConfig(AppConfig):
     name = 'questions'
+
+    def ready(self):
+        import questions.signals
