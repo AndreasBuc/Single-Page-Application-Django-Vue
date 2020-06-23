7b64023bb8b8535251f0802562ea5761cb867bc4
AndreasBuc <63713732+AndreasBuc@users.noreply.github.com>
2020-06-23 19:32:24 +0200

    Zwischenspeicher

---
+++ QuestionTime/frontend/src/components/Navbar.vue
@@ -0,0 +1,59 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+      <a class="navbar-brand" href="#">
+        <router-link
+          :to="{ name: 'home' }"
+          class="navbar-brand"
+          >QuestionTime
+        </router-link></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+        </ul>
+
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <router-link
+              :to="{name: 'home'}"
+              class = "nav-link">
+              Home
+            </router-link>
+          </li>
+
+          <li class="nav-item active">
+            <router-link
+              class = "nav-link"
+              :to="{ name: 'home' }"
+              >Ask Question
+            </router-link>
+          </li>
+
+          <li class="nav-item active">
+            <a class="nav-link" href="/accounts/logout/"
+              >Logout
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavbarComponent"
+};
+</script>
+
+<style>
+  .navbar-brand:hover {
+    color: #f6abb6 !important;
+  }
+  .nav-link:hover {
+    color: #eec9d2 !important;
+  }
+</style>
--- QuestionTime/frontend/src/views/Home.vue
+++ QuestionTime/frontend/src/views/Home.vue
@@ -1,18 +1,62 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App!!!!!" />
+    <div class="container">
+      <div v-for="question in questions" :key="question.pk">
+        <p class="mb-0">Posted by:
+          <span class = "question-author">{{question.author}}</span>
+        </p>
+        <h2>
+          <router-link
+            :to="{ name: 'question', params: {slug: question.slug} }"
+            class="question-link"
+          >{{question.content}}
+          </router-link>
+        </h2>
+        <p>Answers: {{question.answers_count}}</p>
+        <hr>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import { apiService } from "@/common/api.service.js";
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
+  name: "home",
+  data() {
+    return {
+      questions: [],
+      next: null,
+      loadingQuestions: false
+    }
+  },
+  methods: {
+    getQuestions() {
+      // make a GET Request to the questions list endpoint and populate the questions array
+      let endpoint = "/api/questions/";
+      apiService(endpoint)
+      .then(data => {
+        this.questions.push(...data.results)
+      })
+    }
+  },
+  created() {
+    this.getQuestions()
+    document.title = "Question Time"
   }
 };
 </script>
+
+<style media="screen" scoped>
+  .question-author{
+    color: #005b96;
+  }
+  .question-link{
+    color: #011f4b;
+  }
+  .question-link:hover{
+    color: #b3cde0;
+    text-decoration: none;
+  }
+
+</style>
